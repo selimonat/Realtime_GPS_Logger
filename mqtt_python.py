598159c8a33719973ddd7cff3b84f1e5f0db17fb
selimonat <onatselim@gmail.com>
2019-12-24 00:43:09 +0100

    commnted file

--- mqtt_python.py
+++ mqtt_python.py
@@ -1,27 +1,36 @@
 #!/usr/bin/env python3
 
+#Assumes appropriately set mysql db and a mqtt broker
+
 import paho.mqtt.client as mqtt
 import MySQLdb
 import subprocess
 import json
 from geo_utils import *
 
+#read the credential json with user_mqtt, user_mysql, pw_mqtt and pw_mysql
+#fields.
 file = open('credentials.json');
 s=file.read();
 d=json.loads(s)
 
+#assign crediential variables.
 USERNAME_MQTT  =  d["user_mqtt"]
 USERNAME_MYSQL =  d["user_mysql"]
 PW_MQTT        =  d["pw_mqtt"]
 PW_MYSQL       =  d["pw_mysql"]
-                    
+                
+#the name of the table that we will write data
 TABLE_NAME     = "devices"
+
+#creation command for database
 #CREATE TABLE log_owntracks_test2 (time BIGINT UNSIGNED, tid VARCHAR(2), lat
 #DECIMAL(10, 8) NOT NULL, lon DECIMAL(11, 8) NOT NULL, accuracy SMALLINT
 #UNSIGNED, at_home BOOLEAN, distance_home DOUBLE, PRIMARY KEY (time, tid));
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata, flags, rc):
+    #Code to be executed on connect
     print("Connected with result code "+str(rc))
 
     # Subscribing in on_connect() means that if we lose the connection and
@@ -30,13 +39,17 @@ def on_connect(client, userdata, flags, rc):
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-    #print(msg.topic+" "+str(msg.payload))
-    #print(str(msg.payload))
-    #data = json.loads(str(msg.payload))
+    """
+        Each time MQTT server receives a message this is executed.
+        The original json contains many fields we extract only the KEYS
+        from it. We add two new contentn, namely whether a binary telling
+        whether we are at home or not, and Euclidean distance to home.
+        The resulting list is then inserted to the devices.log_owntracks_test2
+    """
+    keys = ['lat', 'lon','tid', 'tst', 'acc']
     my_json = msg.payload.decode('utf8').replace("'", '"')
     #print(my_json)
     data = json.loads(my_json)
-    keys = ['lat', 'lon','tid', 'tst', 'acc']
     data = tuple(data[your_key] for your_key in keys)
     #print(data)
     data = [( *data , is_at_home(data[0],data[1]) , latlon_to_distance( (data[0],data[1]) ) ) ]
